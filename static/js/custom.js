99d19b1129ffca71484dac022ea19e4c1b990e5a
botao <botao@tencent.com>
2022-05-13 16:28:29 +0800

    fix: 主题修改背景图片

--- static/js/custom.js
+++ static/js/custom.js
@@ -9,7 +9,7 @@ function randomColor() {
 /* 轮播背景图片  https://blog.csdn.net/weixin_30794851/article/details/99023580*/
 $(function () {
 	$.backstretch([
-		"/images/lighthouse-large.jpg",
+		// "/images/lighthouse-large.jpg",
 		// "/images/Wavelength.jpg",
 	], { duration: 600, fade: 1500 });
 });
@@ -32,7 +32,7 @@ $(function() {
 	$("div.home-avatar").attr('style', "background: url(https://ghchart.rshah.org/tryturned);background-repeat: no-repeat;background-position: center;background-size: auto 7.5rem;");
 
 	/* 评论框加载背景图片 */
-	$(".v[data-class=v] .veditor").attr('style', "background-image: url(" + $cdnPrefix + "/images/common/valinebg.webp) !important;");
+	$(".v[data-class=v] .veditor").attr('style', "background-image: url(" + "/images/avatar.png) !important;");
 });
 
 function getCurrentDateString() {
@@ -43,46 +43,25 @@ function getCurrentDateString() {
 	return "" + now.getFullYear() + (month < 10 ? "0" + month : month) + (day < 10 ? "0" + day : day) + (hour < 10 ? "0" + hour : hour);
 }
 
-/* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
-window.onload = function() {
-  var OriginTitile = document.title;
-  var titleTime;
-  document.addEventListener('visibilitychange', function() {
-    if(document.hidden) {
-      $('[rel="icon"]').attr('href', "/failure.ico");
-      $('[rel="shortcut icon"]').attr('href', "/failure.ico");
-      document.title = '喔唷，崩溃啦！';
-      clearTimeout(titleTime);
-    } else {
-      $('[rel="icon"]').attr('href', "/favicon-32x32.png");
-      $('[rel="shortcut icon"]').attr('href', "/favicon-32x32.png");
-      document.title = '咦，页面又好了！';
-      titleTime = setTimeout(function() {
-        document.title = OriginTitile;
-      }, 2000);
-	}
-  });
-}
-
 /* 站点运行时间 */
 function runtime() {
 	window.setTimeout("runtime()", 1000);
-    let startTime = new Date('12/24/2021 15:00:00');
-    let endTime = new Date();
-    let usedTime = endTime - startTime;
-    let days = Math.floor(usedTime / (24 * 3600 * 1000));
-    let leavel = usedTime % (24 * 3600 * 1000);
-    let hours = Math.floor(leavel / (3600 * 1000));
-    let leavel2 = leavel % (3600 * 1000);
-    let minutes = Math.floor(leavel2 / (60 * 1000));
-    let leavel3 = leavel2 % (60 * 1000);
-    let seconds = Math.floor(leavel3 / (1000));
-    let runbox = document.getElementById('run-time');
-    runbox.innerHTML = '本站已运行<i class="far fa-clock fa-fw"></i> '
-        + ((days < 10) ? '0' : '') + days + ' 天 '
-        + ((hours < 10) ? '0' : '') + hours + ' 时 '
-        + ((minutes < 10) ? '0' : '') + minutes + ' 分 '
-        + ((seconds < 10) ? '0' : '') + seconds + ' 秒 ';
+  let startTime = new Date('12/24/2021 15:00:00');
+  let endTime = new Date();
+  let usedTime = endTime - startTime;
+  let days = Math.floor(usedTime / (24 * 3600 * 1000));
+  let leavel = usedTime % (24 * 3600 * 1000);
+  let hours = Math.floor(leavel / (3600 * 1000));
+  let leavel2 = leavel % (3600 * 1000);
+  let minutes = Math.floor(leavel2 / (60 * 1000));
+  let leavel3 = leavel2 % (60 * 1000);
+  let seconds = Math.floor(leavel3 / (1000));
+  let runbox = document.getElementById('run-time');
+  runbox.innerHTML = '本站已运行<i class="far fa-clock fa-fw"></i> '
+      + ((days < 10) ? '0' : '') + days + ' 天 '
+      + ((hours < 10) ? '0' : '') + hours + ' 时 '
+      + ((minutes < 10) ? '0' : '') + minutes + ' 分 '
+      + ((seconds < 10) ? '0' : '') + seconds + ' 秒 ';
 }
 
 runtime();
