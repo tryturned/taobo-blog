4a7cfab0bc0023527618f3d477db2f538eec0553
botao <botao@tencent.com>
2022-05-13 14:33:33 +0800

    fix: 样式表更新

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # quickstart
 
 ```bash
-    git clone https://github.com/gohugoio/hugo.git
-    cd hugo
-    go install
+    curl -fLO https://github.com/gohugoio/hugo/releases/download/v0.98.0/hugo_extended_0.98.0_Linux-64bit.tar.gz
+    tar -xzvf hugo_extended_0.98.0_Linux-64bit.tar.gz
+    mv hugo /usr/local/bin
     git clone https://github.com/tryturned/taobo-blog.git
     cd taobo-blog
     git submodule update --init
---
+++ static/js/custom.js
@@ -0,0 +1,89 @@
+/* 
+copy from https://lewky.cn/posts/hugo-3.html/
+*/
+/* 返回随机颜色 */
+function randomColor() {
+	return "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")";
+}
+
+/* 轮播背景图片  https://blog.csdn.net/weixin_30794851/article/details/99023580*/
+$(function () {
+	$.backstretch([
+		"/images/lighthouse-large.jpg",
+		// "/images/Wavelength.jpg",
+	], { duration: 600, fade: 1500 });
+});
+
+/* 拉姆蕾姆回到顶部或底部按钮 */
+$(function() {
+	$("#lamu img").eq(0).click(function() {
+		$("html,body").animate({scrollTop:$(document).height()},800);
+		return false;
+	});
+	$("#leimu img").eq(0).click(function() {
+		$("html,body").animate({scrollTop:0},800);
+		return false;
+	});
+});
+
+/* 后置加载页面组件的背景图片 */
+$(function() {
+	/* 首页头像div加载GitHub Chart作为背景图片 */
+	$("div.home-avatar").attr('style', "background: url(https://ghchart.rshah.org/tryturned);background-repeat: no-repeat;background-position: center;background-size: auto 7.5rem;");
+
+	/* 评论框加载背景图片 */
+	$(".v[data-class=v] .veditor").attr('style', "background-image: url(" + $cdnPrefix + "/images/common/valinebg.webp) !important;");
+});
+
+function getCurrentDateString() {
+	var now = new Date();
+	var month = now.getMonth() + 1;
+	var day = now.getDate();
+	var hour = now.getHours();
+	return "" + now.getFullYear() + (month < 10 ? "0" + month : month) + (day < 10 ? "0" + day : day) + (hour < 10 ? "0" + hour : hour);
+}
+
+/* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
+window.onload = function() {
+  var OriginTitile = document.title;
+  var titleTime;
+  document.addEventListener('visibilitychange', function() {
+    if(document.hidden) {
+      $('[rel="icon"]').attr('href', "/failure.ico");
+      $('[rel="shortcut icon"]').attr('href', "/failure.ico");
+      document.title = '喔唷，崩溃啦！';
+      clearTimeout(titleTime);
+    } else {
+      $('[rel="icon"]').attr('href', "/favicon-32x32.png");
+      $('[rel="shortcut icon"]').attr('href', "/favicon-32x32.png");
+      document.title = '咦，页面又好了！';
+      titleTime = setTimeout(function() {
+        document.title = OriginTitile;
+      }, 2000);
+	}
+  });
+}
+
+/* 站点运行时间 */
+function runtime() {
+	window.setTimeout("runtime()", 1000);
+	/* 请修改这里的起始时间 */
+    let startTime = new Date('04/24/2018 15:00:00');
+    let endTime = new Date();
+    let usedTime = endTime - startTime;
+    let days = Math.floor(usedTime / (24 * 3600 * 1000));
+    let leavel = usedTime % (24 * 3600 * 1000);
+    let hours = Math.floor(leavel / (3600 * 1000));
+    let leavel2 = leavel % (3600 * 1000);
+    let minutes = Math.floor(leavel2 / (60 * 1000));
+    let leavel3 = leavel2 % (60 * 1000);
+    let seconds = Math.floor(leavel3 / (1000));
+    let runbox = document.getElementById('run-time');
+    runbox.innerHTML = '本站已运行<i class="far fa-clock fa-fw"></i> '
+        + ((days < 10) ? '0' : '') + days + ' 天 '
+        + ((hours < 10) ? '0' : '') + hours + ' 时 '
+        + ((minutes < 10) ? '0' : '') + minutes + ' 分 '
+        + ((seconds < 10) ? '0' : '') + seconds + ' 秒 ';
+}
+
+runtime();
